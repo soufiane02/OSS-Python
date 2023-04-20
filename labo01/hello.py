fa66c25f1e4361f689f0d281bbf61e3b786416e4
soufiane <soufiane.elghaddioui@student.ap.be>
2023-04-20 12:57:49 +0200

    ik heb een voorbeeld script geschreven hello.py en ik heb ook een scriptje geschreven dat een nieuw text bestand aanmaakt en automatisch in dat text bestand iets print: write_greeting.py

---
+++ labo01/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+hello_world = "Hello World!"
+
+print(hello_world)
---
+++ labo01/write_greeting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+ff = open("labo01/greeting.txt", "w")
+
+ff.write("Hello World!")
+
+ff.close()
+
+
